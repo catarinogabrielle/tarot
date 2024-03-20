e1eccad7136c1b4775d7ad9a1aa1c6f570c7af11
Gabrielle <gabrielle.castro@ymonetize.com>
2024-03-20 10:42:09 -0300

    at1

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { StatusBar, PermissionsAndroid } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 import { NavigationContainer } from '@react-navigation/native';
 import messaging from '@react-native-firebase/messaging';
-import { StripeProvider } from '@stripe/stripe-react-native';
 
 import Routes from "./src/routes";
 import Available from "./src/pages/Available";
@@ -20,26 +19,21 @@ export default function App() {
 
   const checkAppVersion = async () => {
     try {
-      // Obtenha a versão do aplicativo instalado
       const installedVersion = await DeviceInfo.getVersion()
 
-      // Substitua o URL com o URL real da sua listagem na Play Store
       const playStoreUrl = 'https://play.google.com/store/apps/details?id=com.gabrielledcastro.mobile'
 
-      // Faça uma solicitação à Play Store para obter a versão mais recente
       const response = await fetch(playStoreUrl)
       const html = await response.text()
 
-      // Extraia a versão mais recente do HTML da Play Store (isso pode variar dependendo da estrutura HTML da página)
       const match = html.match(/<div[^>]*>Versão[^<]*<\/div><span[^>]*>([^<]*)<\/span>/)
       const latestVersion = match ? match[1] : null
 
-      // Compare as versões
       if (latestVersion && installedVersion !== latestVersion) {
         console.log('O aplicativo está desatualizado!')
         setAvailable(false)
       } else {
-        // O aplicativo está atualizado
+
         console.log('O aplicativo está atualizado!')
         setAvailable(true)
       }
